e6207fe03f995fd4159071c7d98f1889a0679e04
retsediv <retsediv1@gmail.com>
2019-04-27 15:49:59 +0300

    Add makefile target and some code for initial setup [wip]

---
+++ tests/signature_test.h
@@ -0,0 +1,14 @@
+#include <check.h>
+
+
+/*
+  each entry of this 
+*/
+const char TLV_to_RS_signatures[][2][140] = {
+    {
+        "3044022047efd2bfe73ac36090e91fa78ebb84cafc40ffb7778fa61222260afffedff08302200943567580955e204ee41ed1ba3cfec6f902cfa39c22b3b06aa1de0963e4b863", 
+        "47efd2bfe73ac36090e91fa78ebb84cafc40ffb7778fa61222260afffedff0830943567580955e204ee41ed1ba3cfec6f902cfa39c22b3b06aa1de0963e4b863a5"
+    }
+};
+
+TCase *add_signature_tests(TCase *tc);
