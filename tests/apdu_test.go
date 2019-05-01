9f4ca5d8debc15a02a288cfa40ffb3e4e0040894
retsediv <retsediv1@gmail.com>
2019-05-01 18:37:48 +0300

    Test for addresses

--- tests/apdu_test.go
+++ tests/apdu_test.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"testing"
 	"os"
+	"testing"
 
 	"github.com/RomanMilishchuk/hardware-wallet-go/src/ledger"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	nanos *ledger.NanoS
+	nanos   *ledger.NanoS
 	errOpen error
 )
 
@@ -26,22 +26,36 @@ func TestGetVersion(t *testing.T) {
 	version, errGetVersion := nanos.GetVersion()
 	require.NoError(t, errGetVersion, "Error while getting Skycoin app version")
 
-	// fmt.Println("Skycoin app version: ", version)
+	fmt.Println("Skycoin app version: ", version)
 }
 
 func TestGetPublicKey(t *testing.T) {
 	var account, index uint32
 	account, index = 0x80000000, 0x00000000
 
-	_, errGetPublicKey := nanos.GetPublicKey(account, index)
+	publicKey, errGetPublicKey := nanos.GetPublicKey(account, index)
 	require.NoError(t, errGetPublicKey, "Error while getting public key")
 
-	// fmt.Printf("Skycoin app version (account=%d, index=%d): %s\n", account, index, publicKey)
+	fmt.Printf("Skycoin app version (account=%d, index=%d): %s\n", account, index, publicKey.Hex())
 }
 
+func TestGetAddress(t *testing.T) {
+	var account, index uint32
+	account, index = 0x80000000, 0x00000000
 
-func TestMain(m *testing.M) {	
+	pk, errGetPublicKey := nanos.GetPublicKey(account, index)
+	require.NoError(t, errGetPublicKey, "Error while getting public key")
+
+	address, errGetAddress := nanos.GetAddress(account, index)
+	require.NoError(t, errGetAddress, "Error while getting address")
+
+	require.NoError(t, address.Verify(pk), "Address is not valid for the public key which was returned by ledger")
+
+	fmt.Printf("Address (account=%d, index=%d): %s\n", account, index, address.String())
+}
+
+func TestMain(m *testing.M) {
 	openConnection()
-    code := m.Run() 
-    os.Exit(code)
+	code := m.Run()
+	os.Exit(code)
 }
